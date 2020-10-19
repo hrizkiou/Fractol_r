ac794b04a9e901d06ccf86458d0daff4a3667fb2
Hamza Rizkioui <58826966+hrizkiou@users.noreply.github.com>
2020-10-19 05:05:08 +0100

    Update main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: hrizkiou <hrizkiou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/10 18:39:40 by hrizkiou          #+#    #+#             */
-/*   Updated: 2020/10/17 04:29:56 by hrizkiou         ###   ########.fr       */
+/*   Updated: 2020/10/19 05:01:02 by hrizkiou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,51 +53,64 @@ int				ft_assign(char **av)
 	return (0);
 }
 
+void			ft_init(t_fractal *data)
+{
+	data->mlx_ptr = mlx_init();
+	data->mlx_win = mlx_new_window(data->mlx_ptr, W, H, "Windows");
+	data->img_ptr = mlx_new_image(data->mlx_ptr, W, H);
+	data->img_data = (int *)mlx_get_data_addr(data->img_ptr, &data->bpp, &data->size_l, &data->endian);
+}
+
 int				main(int ac, char **av)
 {
 	t_fractal	*data;
 	data = (t_fractal*)malloc(sizeof(t_fractal));
-	int i = 1;
+	int i;
 	int j;
 	int count;
-	int tmp;
-	data->x1 = -2.1;
-	data->x2 = 0.6;
-	data->y1 = -1.2;
-	data->y2 = 1.2;
-	data->zoom = 200;
+	double tmp;
+	data->zoom = 300;
 	data->it_max = 100;
 	
 	if (ac != 2)
 		ft_error(0);
 	if (strcmp(av[1],"Mandelbrot") == 0)
 	{
-		data->mlx_ptr = mlx_init();
-		data->mlx_win = mlx_new_window(data->mlx_ptr, W, H, "Windows");
-		data->img_ptr = mlx_new_image(data->mlx_ptr, W, H);
-		data->img_data = (int *)mlx_get_data_addr(data->img_ptr, &data->bpp, &data->size_l, &data->endian);
-
-		int	xscale = 0.25 / W; 
-    	int yscale = 0.45 / W; 
-		while (data->y <= W)
+		
+		ft_init(data);
+		
+		
+		i = 0;
+		
+		
+		while (i < W)
 		{
-			while (data->x <= W)
+			j = 0;
+			while (j < W)
 			{
-				data->c_r = data->x * xscale + (-1.75);
-				data->c_i = data->y * yscale + (-0.25);
 				data->z_r = 0;
 				data->z_i = 0;
-				while ((data->z_r * data->z_r + data->z_i * data->z_i < 4) && (count < data->it_max))
+				data->c_r = (double)j / data->zoom + data->x1;
+				data->c_i = (double)i / data->zoom + data->y1;
+				data->it = 0;                                                                                                                                     
+				while (data->z_r * data->z_r + data->z_i * data->z_i < 4 && data->it < data->it_max)
 				{
-					tmp = data->z_r * data->z_r - data->z_i * data->z_i + data->c_r;
-					data->z_i = 2 * data->z_r * data->z_i + data->c_i;
-					data->z_r = tmp;
-					count++;
+					tmp = data->z_r;
+					data->z_r = data->z_r * data->z_r - data->z_i * data->z_i + data->c_r;
+					data->z_i = 2 * tmp * data->z_i + data->c_i;
+					data->it++;
 				}
-				data->x++;
+				if (data->it == data->it_max)
+				{
+					data->img_data[i * W + j / data->zoom] = 0;
+				}
+				else
+				{
+					data->img_data[i * W + j / data->zoom] = 265 * data->it;
+				}
+				j++;
 			}
-			data->y++;
-			printf("Z reel: %d\tZ img: %d\n", data->z_r, data->z_i);
+			i++;
 		}
 		mlx_put_image_to_window(data->mlx_ptr, data->mlx_win, data->img_ptr, 0, 0);
 		mlx_loop(data->mlx_ptr);
